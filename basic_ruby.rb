854385f199644a2fb4064830ddb15dcf0e22e745
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-01-07 11:48:00 -0500

    added basic ruby

---
+++ basic_ruby.rb
@@ -0,0 +1,50 @@
+my_child_age = 2
+#comments
+puts "my child's age is #{my_child_age}"
+#comments etc
+my_age = 32
+puts "my age is #{my_age}"
+#tons of comments
+current_year = 2014
+#all the comments
+puts "current year is #{current_year}"
+#haha! comments!!
+child_dob = current_year - my_child_age
+
+puts "child's Date of Birth = #{child_dob}"
+
+puts "child's Date of Birth = #{current_year - my_child_age}"
+
+my_dob = current_year - my_age
+
+my_dads_age = 57
+
+my_dads_dob = current_year - my_dads_age
+
+my_first_name = "Jen"
+
+my_last_name = "Smith"
+
+my_full_name = my_first_name + " " + my_last_name
+
+puts "my full name is #{my_full_name}"
+
+my_child_age = 2
+
+my_age = 32
+
+current_year = 2014
+
+child_dob = current_year - my_child_age
+
+my_dob = current_year - my_age
+
+my_dads_age = 57
+
+my_dads_dob = current_year - my_dads_age
+
+my_first_name = "Jen"
+
+my_last_name = "Smith"
+
+my_full_name = my_first_name + " " + my_last_name
